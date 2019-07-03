37e265e9ef114b9130103e40c5f7dd638d690db2
Adkidus <adrian648@gmail.com>
2019-07-04 01:57:25 +0200

    * ListViewAdapter.cs: list view
    * MainActivity.cs:
    * projektMobilne.csproj:
    * Resource.designer.cs:
    * listview_row.cs:
    * listview_row.axml:
    * activity_main.axml:

--- projektMobilne/MainActivity.cs
+++ projektMobilne/MainActivity.cs
@@ -3,24 +3,34 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Widget;
+using System.Collections.Generic;
+using Android.Content;
 
 namespace projektMobilne
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private List<string> mItems;
+        private ListView mListView;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
-        }
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
-        {
-            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
-            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+            mListView = FindViewById<ListView>(Resource.Id.friendsView);
+
+            mItems = new List<string>();
+            mItems.Add("Marian");
+            mItems.Add("Janusz");
+            mItems.Add("Wioletta");
+
+            //ArrayAdapter<string> adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleListItem1, mItems);
+
+            ListViewAdapter adapter = new ListViewAdapter(this, mItems);
+
+            mListView.Adapter = adapter;
         }
     }
 }
